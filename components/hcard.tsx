d0a29db7c976808ce6adcfdbce32102a898d17ed
Riley Eileen <52744892+Riley1101@users.noreply.github.com>
2022-02-04 23:23:33 +0630

    updated hover states

--- components/hcard.tsx
+++ components/hcard.tsx
@@ -9,8 +9,8 @@ interface Props {
 const HCard: React.FunctionComponent<Props> = ({ post }) => {
     return (
         <Link href={`/blog/${post.slug}`} passHref={true}>
-            <article className='grid gap-2 lg:gap-2 cursor-pointer transition-transform hover:-translate-x-4 hover:scale-[1.05]' >
-                <h2 className="text-lg font-bold relative lg:text-xl" >{post.title}</h2>
+            <article className='group grid gap-2 lg:gap-2 cursor-pointer transition-transform hover:-translate-x-4 hover:scale-[1.05]' >
+                <h2 className="text-lg font-bold relative lg:text-xl group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-l to-pink-600 from-red-400" >{post.title}</h2>
                 <p className='text-pale-100 text-sm lg:text-[1rem] leading-relaxed'>{post.summary}</p>
             </article>
         </Link>
--- components/vcard.tsx
+++ components/vcard.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const VCard: React.FunctionComponent = () => {
     return (
-        <article className='grid gap-1 text-white aspect-auto  border-2 border-slate-800 p-4 rounded-md cursor-pointer lg:gap-2 hover:scale-[1.01]'>
-            <h2 className="text-lg relative lg:text-xl" >Everything I learnt about Next from my three year experiences.</h2>
+        <article className='group grid gap-1 text-white aspect-auto  border-2 border-slate-800 p-4 rounded-md cursor-pointer lg:gap-2 hover:scale-[1.01]'>
+            <h2 className="text-lg relative lg:text-xl  group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-l to-pink-600 from-red-400" >Everything I learnt about Next from my three year experiences.</h2>
             <p className='text-pale-100 text-sm lg:text-md'>Here’s my over all bugs and stuffs on NextJS</p>
             <span className='mt-2'>Jun 5 , 2021</span>
         </article >
