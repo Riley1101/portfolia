b6aa31a2649f66194d2dbe77fc58818dea2ba37e
Riley Eileen <52744892+Riley1101@users.noreply.github.com>
2022-03-04 22:26:32 +0630

    update on some data

--- pages/api/block/[slug].ts
+++ pages/api/block/[slug].ts
@@ -26,6 +26,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
                         {
                             data: {
                                 slug: slug as string,
+                                blocks: 0
                             },
                         } as any
                     )
