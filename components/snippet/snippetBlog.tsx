0231deca64e1d9636ff791339bd7836424c52702
Riley Eileen <52744892+Riley1101@users.noreply.github.com>
2022-04-07 16:15:23 +0630

    update on snippets

--- components/snippet/snippetBlog.tsx
+++ components/snippet/snippetBlog.tsx
@@ -17,7 +17,6 @@ let meta = {
     image: 'https://firebasestorage.googleapis.com/v0/b/arianagrande-a231e.appspot.com/o/Inspiring%20FRontend%20Developer(1).png?alt=media&token=d50ab1d9-782d-4457-8e63-2f952832f431',
 }
 export default function SnippetPost({ children, snippet }: Props) {
-    console.log(snippet)
     meta.title = `Snippets / ${snippet.title}`
     meta.summary = snippet.summary
     meta.slug = `https://arkardev.space/snippet/${snippet.slug}`
