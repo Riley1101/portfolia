d5acd22b8fc7e18886b53fef2a1f077b49e04487
Riley1101 <www.riley.s.eileen@gmail.com>
2022-04-11 19:49:16 +0630

    update

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,28 +1,49 @@
 import React from 'react';
 import Navigation from '@/components/navigation';
-
 import { allBlogs } from '.contentlayer/data'
-
+import Image from 'next/image'
 import type { Blog } from '.contentlayer/types';
+import type { NextRouter } from 'next/router'
 import NewsLetter from '@/components/newsletter';
 import HCard from '@/components/hcard';
-import MailBox from '@/layouts/mail';
 import Footer from '@/components/footer';
 import { useBlogs } from '@/hooks/dist';
 import Meta from '@/components/blog/meta';
+import categories from '@/data/categories';
+import { useRouter } from 'next/router';
 type Props = {
     posts: Blog[],
-    pinned: Blog[]
 };
 
-export default function Blogs({ posts: cases, pinned }: Props) {
+let meta = {
+    title: 'Inspiring / Blog',
+    summary: 'Personal blog by @arkarkaungmyat',
+    slug: 'arkardev.space/',
+    image: '/static/sharing.png',
+}
+
+export default function Blogs({ posts: cases }: Props) {
     let [keyword, setKeyword] = React.useState('');
-    let { posts: filteredBlogs, pinned: filteredPins } = useBlogs({ posts: cases, pinned, keyword });
-    let meta = {
-        title: 'Inspiring / Blog',
-        summary: 'Personal blog by @arkarkaungmyat',
-        slug: 'arkardev.space/',
-        image: '/static/sharing.png',
+    let [category, setCategory] = React.useState('All')
+    let [more, setMore] = React.useState(false)
+    let { posts: filteredBlogs } = useBlogs({ posts: cases, keyword, category });
+    const router: NextRouter = useRouter();
+    let handleFilter = (ctx: string): void => {
+        setCategory(ctx)
+        if (ctx === 'All') {
+            router.push({
+                pathname: '/blogs',
+
+            })
+        } else {
+            router.push({
+                pathname: '/blogs',
+                query: {
+                    category: ctx
+                }
+            })
+        }
+
     }
     return <div className='page-container'>
         <Meta post={meta} />
@@ -33,21 +54,24 @@ export default function Blogs({ posts: cases, pinned }: Props) {
             <div className='w-full my-4'>
                 <input onChange={(e): void => setKeyword(e.target.value)} type="text" placeholder='Search by name' className='p-2 rounded-md w-full bg-slate-800 px-4 focus:outline-4 focus:outline-slate-400' />
             </div>
-            <div className='mt-8'>
-                <h2 className='block text-2xl font-normal lg:text-3xl lg:font-bold lg:mb-2'>Pinned Blogs</h2>
-                <div className='grid gap-5 py-4 lg:gap-8 lg:mt-0'>
+
+            <div className='my-4 rounded-md flex'>
+                <ul className={`scrollbar-none w-full flex items-center gap-2 overflow-x-scroll transition-all ${more && 'flex-wrap'}`}>
+                    {categories.length > 6 && <li className={`bg-slate-800  min-w-[45px] px-4 py-2 rounded-md cursor-pointer transition-all ${more && `rotate-180`} `}
+                        onClick={() => setMore(!more)}>
+                        <Image src='/static/more.svg' alt='More' width={14} height={14} objectFit='cover' />
+                    </li>}
+                    <li className={`${category === 'All' ? 'bg-slate-900' : 'bg-slate-800'} text-sm px-4 py-2 rounded-md cursor-pointer`} onClick={() => handleFilter('All')}>All</li>
                     {
-                        filteredPins && filteredPins.map((item) => <HCard key={item._id} post={item} />)
+                        categories.map(ctx => <li onClick={() => handleFilter(ctx)} key={ctx} className={`${category === ctx ? 'bg-slate-900' : 'bg-slate-800'}  text-sm px-4 py-2 rounded-md cursor-pointer`}>{ctx}</li>)
                     }
-                </div>
+                </ul>
+
             </div>
-            <div className='mt-8'>
-                <h2 className='block text-2xl font-normal lg:text-3xl lg:font-bold lg:mb-2'>Blogs</h2>
-                <div className='grid gap-5 py-4 lg:gap-8 lg:mt-0'>
-                    {
-                        filteredBlogs && filteredBlogs.map((item) => <HCard key={item._id} post={item} />)
-                    }
-                </div>
+            <div className='grid gap-5 py-4 lg:gap-8 lg:mt-0'>
+                {
+                    filteredBlogs && filteredBlogs.map((item) => <HCard key={item._id} post={item} />)
+                }
             </div>
 
         </div>
@@ -59,7 +83,6 @@ export async function getStaticProps({ locale }: { locale: string }) {
     let tmp = allBlogs.sort((a, b) => {
         return new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
     })
-    const posts = tmp.filter((item: Blog) => item.pinned === false && item.locale === locale);
-    let pinned = tmp.filter((item: Blog) => item.pinned === true && item.locale === locale);
-    return { props: { posts, pinned } };
+    const posts = tmp.filter((item: Blog) => item.locale === locale);
+    return { props: { posts } };
 }
