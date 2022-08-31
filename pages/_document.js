14ba446237f84da79d4517833197d07cd0cb3f71
Riley1101 <www.riley.s.eileen@gmail.com>
2022-08-31 18:59:14 +0630

    updaed google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 // pages/_document.js
-
+import Script from "next/script";
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import theme from "@/utils/theme";
@@ -8,6 +8,21 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+        </Script>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
