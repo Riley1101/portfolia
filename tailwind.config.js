3e1e004ea0090ab87ea9dc7c000fe72c7a712db5
Arkar Kaung Myat <www.riley.s.eileen@gmail.com>
2022-12-03 20:28:59 +0630

    removed tailwind lineclamps

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
   },
   plugins: [
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/typography"),
+    // require("@tailwindcss/line-clamp"),
+    // require("@tailwindcss/typography"),
   ],
 };
