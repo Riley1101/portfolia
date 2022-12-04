bc22d8549ad2ac26e3f99df1a401a0948637ef69
Arkar Kaung Myat <www.riley.s.eileen@gmail.com>
2022-12-04 13:21:07 +0630

    update on image domain

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ["cdn.sanity.io"],
+    domains: ["cdn.sanity.io", "arkardev.space"],
   },
   typescript: {
     // !! WARN !!
