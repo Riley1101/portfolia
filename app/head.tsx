ac36c116abf9cda63d7c084984e657ded3aaa999
Arkar Kaung Myat <www.riley.s.eileen@gmail.com>
2022-12-04 12:52:11 +0630

    update on css

--- app/head.tsx
+++ app/head.tsx
@@ -10,6 +10,9 @@ export default async function Head() {
   };
   return (
     <head>
+      <meta charSet="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <Meta meta={defaultMeta} />
     </head>
   );
