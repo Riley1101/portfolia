d6a5bed088a0d916ef3b52f93d37b7783fc8973e
Arkar Kaung Myat <www.riley.s.eileen@gmail.com>
2022-12-10 19:10:07 +0630

    update on beta domain

--- app/head.tsx
+++ app/head.tsx
@@ -14,6 +14,7 @@ export default async function Head() {
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <Meta meta={defaultMeta} />
+      
     </head>
   );
 }
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -11,3 +11,5 @@ export default function handler(
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+
