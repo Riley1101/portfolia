f80cc82f210af89a11fcc8f976428d1210f487f9
Arkar Kaung Myat <www.riley.s.eileen@gmail.com>
2023-02-06 20:41:36 -0500

    update on embedded urls

--- components/common/portable/katex.tsx
+++ components/common/portable/katex.tsx
@@ -4,6 +4,10 @@ import { BlockMath } from "react-katex";
 function KatexBlock(props: any) {
   const { value } = props;
 
-  return <BlockMath>{value.body}</BlockMath>;
+  return (
+    <div className="block my-6 leading-loose transition-all duration-300 border border-transparent rounded-md bg-theme-accent-opaque bg-gradient-to-r">
+      <BlockMath>{value.body}</BlockMath>
+    </div>
+  );
 }
 export default KatexBlock;
--- components/studio/schemas/index.ts
+++ components/studio/schemas/index.ts
@@ -4,8 +4,10 @@ import article from "./article";
 import author from "./author";
 import snippet from "./snippet";
 import resume from "./resume";
+import youtube from "./youtube";
 
 export const schemaTypes = [
+  youtube,
   resume,
   article,
   author,
