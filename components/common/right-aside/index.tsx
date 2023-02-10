3eb739f9ebcefa67b5792dbbb17e2e04c083f8f9
Riley1101 <www.riley.s.eileen@gmail.com>
2023-02-10 11:20:08 -0500

    added threejs

--- components/common/right-aside/index.tsx
+++ components/common/right-aside/index.tsx
@@ -1,6 +1,9 @@
+import ComputerModel from "../model";
 const RightAside = () => {
   return (
-    <aside className="mt-[5em] hidden col-start-3 lg:flex flex-col "></aside>
+    <aside className="w-full h-full col-start-1  md:col-start-3 mt-[2em] p-4">
+      <ComputerModel />
+    </aside>
   );
 };
 
