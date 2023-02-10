d966ce52481770ded1c3ebe0e78ce3b2c586aeef
Riley1101 <www.riley.s.eileen@gmail.com>
2023-02-10 12:43:12 -0500

    WIP:threejs computer added

--- components/common/model/index.tsx
+++ components/common/model/index.tsx
@@ -1,20 +1,29 @@
 "use client";
-import { Canvas, useLoader } from "@react-three/fiber";
+import { Canvas, PrimitiveProps, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "@react-three/drei";
-import { useRef } from "react";
+import { MutableRefObject, Ref, useRef } from "react";
+import { useObjectMovement } from "hooks/useMouse";
+import { DirectionalLight } from "three";
 
 type Props = {};
 const ComputerModel = (props: Props) => {
+  // change  gltf file to url
   const gltf = useLoader(GLTFLoader, "/models/retro.glb");
-  const light1 = useRef<any>();
-
+  const object: MutableRefObject<PrimitiveProps> =
+    useRef() as MutableRefObject<PrimitiveProps>;
+  const primaryLight: MutableRefObject<DirectionalLight> =
+    useRef<DirectionalLight>() as MutableRefObject<DirectionalLight>;
+  const accentLight: MutableRefObject<DirectionalLight> =
+    useRef<DirectionalLight>() as MutableRefObject<DirectionalLight>;
+  const control: any = useRef();
+  useObjectMovement(object, primaryLight, accentLight);
   return (
-    <div className="w-full h-full border border-theme-accent-opaque aspect-square">
+    <div className="w-full mt-auto border border-theme-accent-opaque aspect-square">
       <Canvas
         className=""
         camera={{
-          position: [50, 70, 110],
+          position: [50, 50, 110],
         }}
       >
         <directionalLight
@@ -22,24 +31,25 @@ const ComputerModel = (props: Props) => {
           position={[-10, 5.0, 3]}
           intensity={0.7}
           castShadow
-          ref={light1}
+          ref={primaryLight}
         />
         <directionalLight
           color={"#f056c8"}
           intensity={0.7}
           position={[20, 5.0, 2]}
           castShadow
+          ref={accentLight}
         />
         {/* <ambientLight color="#88ced3" intensity={0.1} /> */}
         <primitive
+          ref={object}
           object={gltf.scene}
           position={[0, 0, 0]}
-          scale={[0.85, 0.85, 0.85]}
+          scale={[1, 1, 1]}
           children-0-castShadow
         />
 
-        <OrbitControls />
-        <axesHelper args={[5]} />
+        <OrbitControls ref={control} />
       </Canvas>
     </div>
   );
