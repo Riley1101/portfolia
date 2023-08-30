63f9e444c2dacb46682d5d4c84d23842ff8316d8
Riley1101 <arkardev00@gmail.com>
2023-08-30 12:05:35 -0400

    update on intro text

--- components/pages/home/intro/index.tsx
+++ components/pages/home/intro/index.tsx
@@ -4,20 +4,20 @@ export function Intro() {
       <h1 className="text-2xl font-bold text-white">
         Hello, I&apos;m Arkar Dev 👋
       </h1>
-      <p className="text-gray-300 leading-relaxed text-lg">
-        An aspiring software engineer, Coding with a strong passion for design
+      <p className="text-gray-300 leading-relaxed ">
+        An inspiring software engineer, Coding with a strong passion for design
         and computer science, I enjoy creating amazing things and building
         blazingly-fast websites.
       </p>
 
-      <p className="text-gray-300 leading-relaxed text-lg">
+      <p className="text-gray-300 leading-relaxed ">
         Currently working mostly with Typescript, React, and Svelte, as well as
         other JavaScript technologies for now. Here in my blog, I mostly write
         about my experiences and share my thoughts on various topics related to
         software engineering.
       </p>
 
-      <p className="text-gray-300 leading-relaxed mb-4 text-lg">
+      <p className="text-gray-300 leading-relaxed mb-4">
         Over the past years, I have written content for my blog and
         newsletter. I strive to keep things simple by writing about technologies
         that interest me at the time, as well as how I am learning and growing
