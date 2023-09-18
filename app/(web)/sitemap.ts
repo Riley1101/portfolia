5a1e78afbc4a0a2d8d0b6c6cbdb33972a2e52304
Riley1101 <arkardev00@gmail.com>
2023-09-18 12:01:10 -0400

    updated sitemap generator

--- app/(web)/sitemap.ts
+++ app/(web)/sitemap.ts
@@ -1,28 +1,32 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-      {
-        url: 'https://arkar.space',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://arkar.space/about',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://arkar.space/articles',
-        lastModified: new Date(),
-      },
+  return [
+    {
+      url: "https://arkar.space",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://arkar.space/about",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://arkar.space/articles",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
 
-      {
-        url: 'https://arkar.space/snippets',
-        lastModified: new Date(),
-      },
-      {
-        url: 'https://arkar.space/videos',
-        lastModified: new Date(),
-      },
-
-    ];
-  }
+    {
+      url: "https://arkar.space/snippets",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://arkar.space/videos",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+  ];
+}
