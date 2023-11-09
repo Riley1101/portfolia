b04cf134028883853aa0888ce6161ecbf100fbe1
Riley1101 <arkardev00@gmail.com>
2023-11-08 21:57:04 -0500

    upgraded to v14

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  experimental: {
-    serverActions: true,
-  },
   images: {
     domains: ["cdn.sanity.io", "arkar.space", "i.ytimg.com", "burma.social"],
   },
