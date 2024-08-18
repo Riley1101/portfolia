29d7909afbc5992007b11a45293534755ad4ff8d
Arkar Myat <arkardev00@gmail.com>
2024-08-18 12:56:35 -0400

    robot text update

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      allow: "*",
       disallow: "/private/",
     },
     sitemap: "https://arkardev.space/sitemap.xml",
